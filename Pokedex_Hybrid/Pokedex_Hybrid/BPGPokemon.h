7605a2f9413be2a12cd5be225c1d3b47a72167f5
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-20 10:55:37 -0600

    Add / Implement Properties, JSONConvertible, and Initializers to BPGPokemon Model

--- Pokedex_Hybrid/Pokedex_Hybrid/BPGPokemon.h
+++ Pokedex_Hybrid/Pokedex_Hybrid/BPGPokemon.h
@@ -10,4 +10,21 @@
 
 @interface BPGPokemon : NSObject
 
+- (instancetype)initWithName:(NSString *)name
+                  identifier:(NSInteger)identifier
+                   abilities:(NSArray<NSString *> *)abilities NS_DESIGNATED_INITIALIZER;
+
+#pragma mark - Properties
+
+@property (nonatomic, copy, readonly) NSString *name;
+@property (nonatomic, readonly) NSInteger identifier;
+@property (nonatomic, copy, readonly) NSArray<NSString *> *abilities;
+
+@end
+
+
+@interface BPGPokemon (JSONConvertible)
+
+- (instancetype)initWithDictionary:(NSDictionary<NSString *, id> *)dictionary;
+
 @end
--- Pokedex_Hybrid/Pokedex_Hybrid/BPGPokemon.m
+++ Pokedex_Hybrid/Pokedex_Hybrid/BPGPokemon.m
@@ -10,4 +10,48 @@
 
 @implementation BPGPokemon
 
+- (instancetype)init
+{
+    return [self initWithName:@"" identifier:0 abilities:@[]];
+}
+
+- (instancetype)initWithName:(NSString *)name
+                  identifier:(NSInteger)identifier
+                   abilities:(NSArray<NSString *> *)abilities
+{
+    self = [super init];
+    if (self) {
+        _name = [name copy];
+        _identifier = identifier;
+        _abilities = abilities;
+    }
+    return self;
+}
+
+@end
+
+@implementation BPGPokemon (JSONConvertible)
+
+- (instancetype)initWithDictionary:(NSDictionary<NSString *,id> *)dictionary
+{
+    NSString *name = dictionary[@"name"];
+    NSInteger identifier = [dictionary[@"id"] integerValue];
+    NSArray *abilityDictionaries = dictionary[@"abilities"];
+    
+    if (![name isKindOfClass:[NSString class]] || ![abilityDictionaries isKindOfClass:[NSArray class]]) {
+        return nil;
+    }
+    
+    NSMutableArray<NSString *> *abilities = [[NSMutableArray alloc] init];
+    
+    for (NSDictionary *dictionary in abilityDictionaries) {
+        
+        NSString *abilityName = dictionary[@"ability"][@"name"];
+        if (!abilityName) {continue; }
+        [abilities addObject:abilityName];
+    }
+    
+    return [self initWithName:name identifier:identifier abilities:abilities];
+}
+
 @end
